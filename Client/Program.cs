3a299408cf7de4291e41dc6db7adc9335f081ace
陈弟CD <bbigcd@163.com>
2019-02-28 14:44:02 +0800

    Initial commit

---
+++ Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+
+            var disco = client.GetDiscoveryDocumentAsync("http://localhost:5000").Result;
+
+            if(disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            var tokenResponse = client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1"
+            }).Result;
+
+            if(tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;                
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            var apiClient = new HttpClient();
+            apiClient.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = apiClient.GetAsync("http://localhost:5001/identity").Result;
+            if(!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# IdentityServer4-Demo
+
+## 照着这篇文章，用vscode过一遍IdentityServer4的例子
+
+[IdentityServer4（7）- 使用客户端认证控制API访问（客户端授权模式）](http://www.cnblogs.com/stulzq/p/7495129.html)
